ff608b37407d14a4121b5eee4928848e4b9da4f4
shalonningram1 <shalonn.ingram@cloudreach.com>
2022-03-24 14:31:40 -0400

    Setting up backend to work with terraform

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "my_project_bucket" {
+    bucket = "talent-academy-tf-backend-shalonn"
+
+    lifecycle {
+      prevent_destroy = true
+    }
+
+    tags = {
+        Name = "talent-academy-tf-backend-shalonn"
+        Environment = "Test"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "version_my_bucket" {
+  bucket = aws_s3_bucket.my_project_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_lock_tbl" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags           = {
+    Name = "terraform-lock"
+  }
+}
