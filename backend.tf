add67b4e403cc980b37dd9dc7aa26e2ec2482cd3
shalonningram1 <shalonn.ingram@cloudreach.com>
2022-03-25 08:59:39 -0400

    Editing my backend

--- backend.tf
+++ backend.tf
@@ -2,14 +2,6 @@ terraform {
   backend "s3" {
     bucket = "talent-academy-tf-backend-shalonn"
     key    = "sprint1/week2/training-terraform/terraform.tfstates"
-  }
-}
-
-
-terraform {
-  backend "s3" {
-    bucket = "talent-academy-account_id-tfstates-pascal"
-    key    = "sprint1/week2/training-terraform/terraform.tfstates"
     dynamodb_table = "terraform-lock"
   }
 }
